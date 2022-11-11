3b4364897c27d58f7980d2787832b00a572167e2
arko <arkajyotisaha891@gmail.com>
2022-11-11 16:30:11 +0530

    🏆 Weekly Contest 311

--- Weekly Contest 311/D.cpp
+++ Weekly Contest 311/D.cpp
@@ -1,1 +1,30 @@
 // 2416. Sum of Prefix Scores of Strings
+class Trie {
+    public:
+        Trie *ch[26] = {};
+        int visited = 0;
+};
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie trie;
+        vector<int> ans;
+        for (string& x: words) {
+            auto t = &trie;
+            for (char& c: x) {
+                if (!t->ch[c - 'a']) t->ch[c - 'a'] = new Trie();
+                t->ch[c - 'a']->visited++;
+                t = t->ch[c - 'a'];
+            }
+        }
+        for (string& x: words) {
+            auto t = &trie; int curr = 0;
+            for (char& c: x) {
+                curr += t->ch[c - 'a']->visited;
+                t = t->ch[c - 'a'];
+            }
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
