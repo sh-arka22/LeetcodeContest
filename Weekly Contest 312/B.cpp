1cf88ce86dbdff9ab8149688e0c5a52643507af3
arko <arkajyotisaha891@gmail.com>
2022-11-11 14:11:42 +0530

    🏆 Weekly Contest 312

---
+++ Weekly Contest 312/B.cpp
@@ -0,0 +1,20 @@
+// 2419. Longest Subarray With Maximum Bitwise AND
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int N = nums.size();
+        int maxEle = *max_element(nums.begin(), nums.end());
+        int subarrayLen = 0, ans = 1;
+        
+        for(int i = 0; i < N; i++) {
+            if (nums[i] == maxEle) {
+                subarrayLen++;
+                ans = max(ans, subarrayLen);
+            } 
+            else {
+                subarrayLen = 0;
+            }
+        }
+        return ans;
+    }
+};
