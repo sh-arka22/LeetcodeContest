1f33999fc811e1d5256835330a00b619b065f9fe
arko <arkajyotisaha891@gmail.com>
2023-03-09 12:25:30 +0530

    🏆 Weekly Contest 327

---
+++ Weekly Contest 327/B.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+//ceil(n /k) = (n+(k-1))/k
+    long long maxKelements(vector<int>& nums, int k) {
+        long long res = 0;
+        priority_queue<int> pq(begin(nums), end(nums));
+        while (k-- && !pq.empty()) {
+            int n = pq.top(); pq.pop();
+            res += n;
+            if ((n + 2) / 3)
+                pq.push((n + 2) / 3);
+        }
+        return res;
+    }
+};
